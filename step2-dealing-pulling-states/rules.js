fb934a1d965b53cc9cb89b86eafd58c9d301bffe
Ji7hotdoeg <37932493+daniel930324@users.noreply.github.com>
2025-04-18 22:03:15 -0700

    adds the general framework functions for rules.js

---
+++ step2-dealing-pulling-states/rules.js
@@ -0,0 +1,29 @@
+// Calculates total value of a hand with correct ace handling
+// note: Blackjack rules: face = 10, Ace = 11 or 1
+export function calculateHandValue(hand) {
+    // todo
+}
+
+// Checks if hand is a natural blackjack (exactly 2 cards = 21)
+export function isBlackjack(hand) {
+    // todo
+}
+
+// Checks if the hand has exceeded 21.
+export function isBust(hand) {
+    // todo
+}
+
+//Determines the game result.
+// Use hand value and rules to decide winner
+export function evaluateHands(playerHand, dealerHand) {
+    // todo
+}
+
+
+// should I add the dealer ai here?
+/**
+export function dealerShouldHit(hand){
+    // todo
+}
+ */
