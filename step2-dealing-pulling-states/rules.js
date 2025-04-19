93a08c2a4e03003dfd5555dc2963720734b6c7f5
Ji7hotdoeg <37932493+daniel930324@users.noreply.github.com>
2025-04-18 23:01:17 -0700

    finish the function calculateHandValue

--- step2-dealing-pulling-states/rules.js
+++ step2-dealing-pulling-states/rules.js
@@ -1,14 +1,42 @@
 // Calculates total value of a hand with correct ace handling
 // note: Blackjack rules: face = 10, Ace = 11 or 1
 export function calculateHandValue(hand) {
-    // todo
-    // consider the flowers inequality
+    let handValue = 0;
+    let aces = 0;
+
+    for(const card of hand){
+        const rank = card.rank;
+
+        if(rank === 'A'){
+            sum += 11;
+            aces += 1;
+        } else if (rank === 'K' || rank === 'Q' || rank === 'J') {
+            sum += 10;
+        } else {
+            sum += Number(rank);
+        }
+    }
+
+    // to adapt the rule that an ace can both be 1 or 11
+    while(sum > 21 && aces > 0) {
+        aces -= 1;
+        sum -= 10; // to replace the value of 11 to 1
+    }
+
+    return sum;
 }
 
 // Checks if hand is a natural blackjack (exactly 2 cards = 21)
 export function isBlackjack(hand) {
     const handValue = calculateHandValue(hand);
     if (hand.length === 2 && handValue ===21){
+        return true;
+    }
+}
+
+// 5 cards charlie rule
+export function winByCharlieRule(hand){
+    if(hand.length >= 5){
         return true;
     }
 }
